b687468d9977fe86a49ad60d154542d04ea782bd
kfir hoftman <kfir.hoftman@gmail.com>
2023-06-14 18:20:00 +0300

    done working on header and footer looks

---
+++ frontend/views/partials/Header/Header.js
@@ -0,0 +1,6 @@
+function resetPlaceholder() {
+  var input = document.querySelector("#searchBar input");
+  input.value = "";
+  input.placeholder = "Search Here..";
+  window.location.href = "/frontend/views/layouts/ProductPage/ProductPage.html";
+}

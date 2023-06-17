8392a2d2e4c5954b14c227b85407c1e17878b6d6
kfir hoftman <kfir.hoftman@gmail.com>
2023-06-17 21:14:17 +0300

    updated products page looks and functionality

---
+++ frontend/views/layouts/ShopPage/ShopPage.js
@@ -0,0 +1,73 @@
+$(function () {
+  $("#header").load("/frontend/views/partials/Header/Header.html");
+  $("#footer").load("/frontend/views/partials/Footer/Footer.html");
+});
+
+const shopBySelect = document.getElementById("shopBy");
+var productItems = document.querySelectorAll("#productCell");
+var productsPrices = document.querySelectorAll(".product-price");
+const sortBySelect = document.getElementById("sortBy");
+const productElements = document.getElementsByClassName("box");
+const originalArray = Array.from(productElements);
+
+shopBySelect.addEventListener("change", function (event) {
+  event.preventDefault();
+
+  const selectedOption = event.target.value;
+
+  for (let i = 0; i < productItems.length; i++) {
+    if (selectedOption == "all") {
+      productItems[i].style.display = "";
+    } else if (productItems[i].classList.contains(selectedOption)) {
+      productItems[i].style.display = "";
+    } else {
+      productItems[i].style.display = "none";
+    }
+  }
+});
+
+sortBySelect.addEventListener("change", function (event) {
+  event.preventDefault();
+  const selectedOption = event.target.value;
+  console.log(selectedOption);
+
+  function comparePrices(a, b) {
+    const priceA = parseFloat(
+      a.querySelector(".product-price").textContent.replace("$", "")
+    );
+    const priceB = parseFloat(
+      b.querySelector(".product-price").textContent.replace("$", "")
+    );
+    if (selectedOption == "least") {
+      return priceA - priceB;
+    } else if (selectedOption == "most") {
+      return priceB - priceA;
+    }
+  }
+
+  if (selectedOption !== "none") {
+    const sortedArray = Array.from(productElements).sort(comparePrices);
+
+    const parentElement = productElements[0].parentElement;
+    Array.from(productElements).forEach(function (product) {
+      parentElement.removeChild(product);
+    });
+
+    sortedArray.forEach(function (product) {
+      parentElement.appendChild(product);
+    });
+  } else {
+    resetOrder();
+  }
+});
+
+function resetOrder() {
+  const parentElement = productElements[0].parentElement;
+  Array.from(parentElement.children).forEach(function (child) {
+    parentElement.removeChild(child);
+  });
+
+  originalArray.forEach(function (product) {
+    parentElement.appendChild(product);
+  });
+}

ad794137f2ef0ea118578c3ff1b96bd8d91b033c
Etaizil <etai444@gmail.com>
2023-06-18 19:50:33 +0300

    Fix cr, change payment method logic

--- frontend/views/layouts/Checkout/Checkout.js
+++ frontend/views/layouts/Checkout/Checkout.js
@@ -3,20 +3,13 @@ $(function () {
   $("#footer").load("/frontend/views/partials/Footer/Footer.html");
 });
 
-function togglePaymentDescription() {
-  var paymentMethods = document.getElementsByName("payment-method");
-  var descriptions = {
-    "credit-card": document.getElementById("credit-card-description"),
-    "cash-on-delivery": document.getElementById("cash-on-description"),
-    paypal: document.getElementById("paypal-description"),
-  };
-
-  for (var i = 0; i < paymentMethods.length; i++) {
-    var paymentMethod = paymentMethods[i];
-    if (descriptions.hasOwnProperty(paymentMethod.value)) {
-      descriptions[paymentMethod.value].style.display = paymentMethod.checked
-        ? "block"
-        : "none";
-    }
-  }
-}
+$(document).ready(function () {
+  $(".selection").hide();
+  $('input[name="payment-method"]').each(function (index, element) {
+    $(element).on("click", function () {
+      var descriptionId = "#" + $(this).val() + "-description";
+      $(".selection").hide();
+      $(descriptionId).show();
+    });
+  });
+});

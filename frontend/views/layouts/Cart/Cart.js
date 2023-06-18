36ac59c85aa5e0c57cfb854ca63ad5e49089794d
Etaizil <etai444@gmail.com>
2023-06-18 19:13:01 +0300

    Fix cr, add increment/decrement logic

--- frontend/views/layouts/Cart/Cart.js
+++ frontend/views/layouts/Cart/Cart.js
@@ -2,3 +2,18 @@ $(function () {
   $("#header").load("/frontend/views/partials/Header/Header.html");
   $("#footer").load("/frontend/views/partials/Footer/Footer.html");
 });
+
+$(function () {
+  $(".minus-button").click(function () {
+    let value = parseInt($(this).siblings(".counter-value").text());
+    if (value > 1) {
+      value--;
+      $(this).siblings(".counter-value").text(value);
+    }
+  });
+  $(".plus-button").click(function () {
+    let value = parseInt($(this).siblings(".counter-value").text());
+    value++;
+    $(this).siblings(".counter-value").text(value);
+  });
+});

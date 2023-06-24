b52f4a41c24a5876ffe594ff8a8ad139862bea0d
kfir hoftman <kfir.hoftman@gmail.com>
2023-06-24 21:17:41 +0300

    updated schema for products. added 'required' field

--- backend/models/products/product.schema.js
+++ backend/models/products/product.schema.js
@@ -1,32 +1,33 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  product: [
-    {
-      productName: {
-        type: String,
-        validate: function (String) {
-          return length(String) > 0;
-        },
-        message: "Name must be a non-empty string",
-      },
-
-      productPrice: {
-        type: Number,
-        min: [1, "The minimum price is 1 dollar."],
-      },
-      productDescription: {
-        type: String,
-      },
-      productCategory: {
-        type: String,
-        enum: ["machines", "capsules", "beans", "accessories"],
-      },
-      productImage: {
-        type: String,
-      },
+  productName: {
+    type: String,
+    validator: function (name) {
+      return name.length > 0;
     },
-  ],
+    message: "Name must be a non-empty string",
+    required: true,
+  },
+
+  productPrice: {
+    type: Number,
+    min: [1, "The minimum price is 1 dollar."],
+    required: true,
+  },
+  productDescription: {
+    type: String,
+    required: true,
+  },
+  productCategory: {
+    type: String,
+    enum: ["machines", "capsules", "beans", "accessories"],
+    required: true,
+  },
+  productImage: {
+    type: String,
+    required: true,
+  },
 });
 
 const Products = mongoose.model("products", ProductSchema);

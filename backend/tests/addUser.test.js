4e1b69f6978a535833bd1bf5ffc6fb81ab474de2
unknown <barara98@gmail.com>
2023-06-26 15:27:01 +0300

    connect mongo to the database

--- backend/tests/addUser.test.js
+++ backend/tests/addUser.test.js
@@ -1,26 +1,25 @@
 async function main() {
-    
-    require('dotenv').config({ path: '../.env' });
-    const mongoConnect = require('../config/mongoConnect')
-    const connection = await mongoConnect();
-    console.log(connection);
-    const User = require('../models/users/user.schema')
-    const user = await User({
-        firstName: "Gil",
-        lastName: "Hlaer",
-        password: "1234",
-        rePassword: "1234",
-        email: "abc1@ads.com",
-        phone: "1234567890",
-        country: "Israel",
-        city: "Rishon LeZion",
-        street: "Elie Wiesel",
-        houseNumber: "2",
-        type: 'admin'
-    })
-    const savedUser = await user.save()
-    console.log(savedUser);
-    return
+  require("dotenv").config({ path: "../.env" });
+  const mongoConnect = require("../config/mongoConnect");
+  const connection = await mongoConnect();
+  console.log(connection);
+  const User = require("../models/users/user.schema");
+  const user = await User({
+    firstName: "Gil",
+    lastName: "Hlaer",
+    password: "123743",
+    rePassword: "123743",
+    email: "17@ads.com",
+    phone: "1234567890",
+    country: "Israel",
+    city: "Rishon LeZion",
+    street: "Elie Wiesel",
+    houseNumber: "2",
+    type: "admin",
+  });
+  const savedUser = await user.save();
+  console.log(savedUser);
+  return;
 }
 
-main()
+main();

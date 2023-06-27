ebc3f0226cc9dc153773cc42a6bf8abcf8e851ae
Etaizil <etai444@gmail.com>
2023-06-27 20:24:15 +0300

    Created Cart.ejs and moved code

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const mongoConnect = require('./config/mongoConnect.js');
-const session = require('express-session');
-const path = require('path')
-const routes = require('./routes');
+const express = require("express");
+const mongoConnect = require("./config/mongoConnect.js");
+const session = require("express-session");
+const path = require("path");
+const routes = require("./routes");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT;
-const SECRETE = process.env.SECRETE
+const SECRETE = process.env.SECRETE;
 
-
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'views')));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "views")));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(session({
+app.use(
+  session({
     secret: SECRETE,
     resave: false,
-    saveUninitialized: false
-  }));
-app.use('/signup', routes.signupRouter);
-app.use('/login', routes.loginRouter);
-app.use('/account', routes.accountRouter);
-
+    saveUninitialized: false,
+  })
+);
+app.use("/signup", routes.signupRouter);
+app.use("/login", routes.loginRouter);
+app.use("/account", routes.accountRouter);
+app.use("/cart", routes.cartRouter);
 
 app.listen(PORT, () => {
-    console.log(`app available on http://localhost:${PORT}`);
-    mongoConnect();
+  console.log(`app available on http://localhost:${PORT}`);
+  mongoConnect();
 });
-  

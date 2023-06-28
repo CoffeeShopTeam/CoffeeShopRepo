dc4fbc907d0f5101ec8989400912f6acf805c09b
kfir hoftman <kfir.hoftman@gmail.com>
2023-06-28 22:49:24 +0300

    ShopPage is now in ejs form

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,11 @@ const signupRouter = require("./signup.routes");
 const loginRouter = require("./login.routes");
 const accountRouter = require("./account.routes");
 const productRouter = require("./products.routes");
-
+const shopRouter = require("./shopPage.routes");
 module.exports = {
   signupRouter,
   loginRouter,
   accountRouter,
   productRouter,
+  shopRouter,
 };
---
+++ backend/routes/shopPage.routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const productsController = require(path.join(
+  __dirname,
+  "../",
+  "controller",
+  "products",
+  "products.controller"
+));
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const products = await productsController.getAllProducts(res);
+//     res.render(path.join(__dirname, "../", "views", "ShopPage", "ShopPage"), {
+//       products,
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await productsController.getAllProducts();
+    res.render("ShopPage/ShopPage", { products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = router;

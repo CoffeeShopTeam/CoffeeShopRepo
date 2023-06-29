6aba5417676af0269d9e576b4060a015ee63950a
Etaizil <etai444@gmail.com>
2023-06-29 17:42:49 +0300

    solve conflicts after pull dev

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 const signupRouter = require("./signup.routes");
 const loginRouter = require("./login.routes");
 const accountRouter = require("./account.routes");
+const checkoutRouter = require("./checkout.routes");
+const productRouter = require("./products.routes");
+const shopRouter = require("./shopPage.routes");
 const cartRouter = require("./cart.routes");
 module.exports = {
   signupRouter,
   loginRouter,
   accountRouter,
+  checkoutRouter,
+  productRouter,
+  shopRouter,
   cartRouter,
 };
-=======
-const signupRouter = require('./signup.routes');
-const loginRouter = require('./login.routes');
-const accountRouter = require('./account.routes');
-const checkoutRouter = require('./checkout.routes');
-const productRouter = require("./products.routes");
-const shopRouter = require("./shopPage.routes");
-module.exports = {
-    signupRouter,
-    loginRouter,
-    accountRouter,
-    checkoutRouter,
-    productRouter,
-    shopRouter,
-}
->>>>>>> 7a6fa7868ae4a05525e53b3bbe5d59e3c6db3b0a

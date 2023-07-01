41469790857ba36c191b9268005dc9657ed6fe7d
Etaizil <etai444@gmail.com>
2023-07-01 16:03:54 +0300

    change style in css

--- backend/views/checkout/Checkout.js
+++ backend/views/checkout/Checkout.js
@@ -8,28 +8,31 @@ $(function () {
     allProducts.each(async function (i, product) {
       const amount = $(product).find("h5").attr("data-amount");
       const baseCurrency = $(product).find("span").attr("currency-select");
-      // fetch
-      const res = await axios.get(`/checkout/exchange/`, {
-        params: {
-          baseCurrency,
-          targetCurrency,
-          amount,
-        },
-      });
-      const data = res.data;
-      console.log(`the data is  ${data}`, data);
-      $(product).find("span").attr("currency-select", data.current);
-      $(product).find("h5").attr("data-amount", data.convertedAmount);
-      $(product)
-        .find(".currency-symbol")
-        .text(function () {
-          return `${data.convertedAmount} ${data.currencySymbol}`;
+      try {
+        // Fetch exchange rate
+        const res = await axios.get(`/checkout/exchange/`, {
+          params: {
+            baseCurrency,
+            targetCurrency,
+            amount,
+          },
         });
+        const data = res.data;
+        $(product).find("span").attr("currency-select", data.currency);
+        $(product).find("h5").attr("data-amount", data.convertedAmount);
+        $(product)
+          .find(".currency-symbol")
+          .text(function () {
+            return `${data.convertedAmount} ${data.currencySymbol}`;
+          });
+      } catch (error) {
+        console.error("Error performing currency conversion:", error);
+      }
     });
   });
+
   $(".container").on("submit", function (event) {
     let orderPrice = $('h5[name="orderPrice"]').data("amount");
-
     $("<input>")
       .attr({
         type: "hidden",
@@ -40,10 +43,9 @@ $(function () {
   });
 });
 
-let checkoutProducts = JSON.parse(localStorage.getItem("jsonProducts"));
-console.log(checkoutProducts);
-
 $(document).ready(function () {
+  let checkoutProducts = JSON.parse(localStorage.getItem("jsonProducts"));
+  console.log(checkoutProducts);
   cart = getProducts();
   populateCheckout(cart);
   setTotalValue(cart);
@@ -57,25 +59,27 @@ $(document).ready(function () {
       addProductLine(item);
     });
   }
+
   function getProducts() {
     let cart = JSON.parse(localStorage.getItem("jsonProducts"));
     return cart;
   }
+
   function addProductLine(cartItem) {
     let item = $("#checkout-product");
     const newItem = $(
       "<div class='d-flex justify-content-between align-items-center mb-2 product'>"
     );
     newItem.html(`
-            <h5 class="mb-0" data-amount="${
-              cartItem.price * cartItem.quantity
-            }">${cartItem.title}</h5>
-            <span class="quantity">${cartItem.quantity}</span>
-            <span class="currency-symbol" currency-select="ILS">${
-              cartItem.price * cartItem.quantity
-            }₪</span>
-        <hr class="mb-3" />
-        `);
+      <h5 class="mb-0" data-amount="${
+        cartItem.price * cartItem.quantity
+      }">${cartItem.title}</h5>
+      <span id="quantity">${cartItem.quantity}</span>
+      <span class="currency-symbol" currency-select="ILS">${(
+        cartItem.price * cartItem.quantity
+      ).toFixed(2)}₪</span>
+      <hr class="mb-3" />
+    `);
     item.append(newItem);
     setTotalValue(cart);
   }
@@ -85,29 +89,31 @@ $(document).ready(function () {
     if (cart === null) {
       countTotal = 0.0;
       let cartTotalContainer = $("#checkout-total");
-      cartTotalContainer.html(`<div class="d-flex justify-content-between align-items-center mb-2 product">
-            <h5 class="checkout-total mb-0" data-amount="${
-              cartItem.price
-            }" name="orderPrice">TOTAL</h5>
-            <span class="currency-symbol" currency-select="ILS">${countTotal.toFixed(
-              2
-            )}₪</span>
-        </div>`);
+      cartTotalContainer.html(`
+        <div class="d-flex justify-content-between align-items-center mb-2 product">
+          <h5 class="checkout-total mb-0" data-amount="${countTotal.toFixed(
+            2
+          )}" name="orderPrice">TOTAL</h5>
+          <span class="currency-symbol" currency-select="ILS">${countTotal.toFixed(
+            2
+          )}₪</span>
+        </div>
+      `);
     } else {
       cart.forEach(function (item) {
         countTotal += item.price * item.quantity;
       });
       let cartTotalContainer = $("#checkout-total");
-      cartTotalContainer.html(
-        `<div class="d-flex justify-content-between align-items-center mb-2 product">
-            <h5 class="checkout-total mb-0" data-amount="${countTotal.toFixed(
-              2
-            )}" name="orderPrice">TOTAL</h5>
-            <span class="currency-symbol" currency-select="ILS">${countTotal.toFixed(
-              2
-            )}₪</span>
-        </div>`
-      );
+      cartTotalContainer.html(`
+        <div class="d-flex justify-content-between align-items-center mb-2 product">
+          <h5 class="checkout-total mb-0" data-amount="${countTotal.toFixed(
+            2
+          )}" name="orderPrice">TOTAL</h5>
+          <span class="currency-symbol" currency-select="ILS">${countTotal.toFixed(
+            2
+          )}₪</span>
+        </div>
+      `);
     }
   }
 });
